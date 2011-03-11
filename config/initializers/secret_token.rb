02b1758bceda754dc4331a8556879435ea518280
Brian Jordan <bcjordan@gmail.com>
2011-03-10 19:17:26 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NationalHistoricPlaces::Application.config.secret_token = 'fc9691dd8cb9adf2949d14c1e749170560705dc78ac2c8807764102bce17a0aaac9be9dc1f150ea017afe039af8e11ab5db51e35b66f442f5ed2004e7a592181'
